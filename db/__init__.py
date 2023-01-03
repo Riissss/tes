5289085f7baaa0e4a33f22f7579558e1df376feb
Riissss <121730144+Riissss@users.noreply.github.com>
2023-01-03 13:55:45 +0700

    Add files via upload

---
+++ db/__init__.py
@@ -0,0 +1,7 @@
+from .make import make
+from .insert import insert
+from .column import column
+from .row import row
+from .del_row import del_row
+from .count import count
+from .update import update
